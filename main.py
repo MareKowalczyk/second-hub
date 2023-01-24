92ae2d59c1f6420f65b9f4824cd22dd8e5dc1fc3
Marek Kowalczyk <mkowalczykm2@gmail.com>
2023-01-24 19:28:50 +0100

    Updated status

--- main.py
+++ main.py
@@ -5,3 +5,9 @@
 # 5. git push 
 # 6. git pull
 # 7. git status
+
+# 1. git branch - to check branches
+# 2. git branch *name* - to create new branch
+# 3. git checkout *name* - to change branch
+# 4. ...
+# 5. *Work as in normal branch*

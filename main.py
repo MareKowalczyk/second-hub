e978b35d219b36a075fec4815c85d483dcd33e4d
Marek Kowalczyk <mkowalczykm2@gmail.com>
2023-01-24 19:02:30 +0100

    Updating

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-# This is something here
-#Some more stuff from computer here
-#This is a new edit from new comptuter
+# 1. git clone *link*
+# 2. ...
+# 3. git add *file_name* lub . 
+# 4. git commit -m '*message*'
+# 5. git push 
+# 6. git pull
+# 7. git status
